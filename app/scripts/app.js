b52ed27b3e06038c1ceb6a73c2d040b638ab3c51
Alexander Petrovich <chopachom@gmail.com>
2013-08-05 14:22:16 +0400

    feat(directive): working basic file upload

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('gridfsFrontendApp', ['dndUpload'])
+angular.module('gridfsFrontendApp', ['dropzoneUpload'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- src/dndupload.js
+++ src/dndupload.js
@@ -1,17 +1,22 @@
 'use strict';
 
-angular.module('dndUpload', []).directive('dndUpload', function(){
+angular.module('dropzoneUpload', []).directive('dropzone', function($browser){
   return {
     restrict: 'A',
     link: function(scope, elem, attrs){
       new Dropzone(elem[0], {
         url: attrs.url,
         maxFileSize: attrs.maxsize,
+        method: 'PUT',
+        headers: {'X-XSRF-TOKEN': $browser.cookies()['XSRF-TOKEN']},
         init: function(){
           this.on("addedfile", function(file){
             if(angular.isArray(scope.files)){
               scope.files.push(file);
             }
+          });
+          this.on('success', function(file, response){
+            console.log(file, response);
           })
         }
       });
